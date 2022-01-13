2c91d5b593fe5825d2e6409aaa0a18be1722163d
Jeff Dyer <jeffdyer@acm.org>
2022-01-13 09:18:06 -0800

    Fix build

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
